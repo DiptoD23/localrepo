ebd8e94cc2fb60116c0601656466e9e9b6677040
DiptoD23 <diptodebnath2003@gmail.com>
2026-09-23 17:06:40 +0600

    added a c file

---
+++ C_codes/Square_Root.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter a positive number: ");
+    scanf("%d", &n);
+    float y = (float)n;
+
+    if(n == 0 || n == 1 ) printf("%d", n);
+    else{
+        for(int i = 1; i < 20; i++){
+            y = (y + n / y) / 2;
+        }
+    }
+
+    printf("Square root of %d is %f", n, y);
+    return 0;
+}
